8147d8c0cc2a69150c3a9901fcfd0cb603caa7c5
kik0s <kostya.amelichev@gmail.com>
2021-02-01 11:15:35 +0000

    telegram bot added:

--- main.py
+++ main.py
@@ -1,11 +1,54 @@
 from markdown2 import Markdown
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import os
 import traceback 
+import requests
+import json
+from telegram import Bot, ParseMode
+from os import system
 
 app = Flask("KiKoS")
 markdowner = Markdown() 
 
+token_TG = json.load(open('secret_data.json', 'r'))['token_TG']
+channel_ID = int(json.load(open('secret_data.json', 'r'))['channel_ID'])
+
+def send_text(text):
+    bot = Bot(token_TG)
+    bot.send_message(channel_ID, text)
+
+def send_photo(photo, caption_ = ''):
+    bot = Bot(token_TG)
+    bot.send_photo(channel_ID, photo, caption = caption_, parse_mode=ParseMode.HTML)
+
+@app.route("/admin/telegram", methods=['POST'])
+def index():
+    received_json_data=json.loads(request.data.decode('utf-8'))
+    if received_json_data['type'] == 'confirmation':
+        return '57be742b'
+    elif received_json_data['type'] == 'wall_post_new':
+        text = ''
+        if 'text' in received_json_data['object']:
+            text = received_json_data['object']['text']
+        photo_url = ''
+        try:
+            if 'attachments' in received_json_data['object']:
+                best_photo_size = ['photo_1280', 'photo_807', 'photo_604', 'photo_75']
+                photo = received_json_data['object']['attachments'][0]['photo']
+                for c in best_photo_size:
+                    if c in photo:
+                        photo_url = photo[c]
+                        break
+        except:
+            pass
+        if photo_url == '':
+            send_text(text)
+        else:
+            send_text(text)
+            try:
+                send_photo(photo_url, caption_ = 'Комментировать лучше в дискасс @chat_27325')
+            except:
+                pass
 
 @app.route("/")
 def hello():
@@ -25,14 +68,14 @@ def reload():
         if name.endswith('.md'):
             try:
                 print(name)
-                with open('static/markdown/' + name, 'r') as fin:
-                    with open('static/html/' + name[:-2] + 'html', 'w') as fout:
-                        fout.write(markdowner.convert(fin.read()))
+                with open('static/markdown/' + name, 'r+') as fin:
+                    with open('static/html/' + name[:-2] + 'html', 'w+') as fout:
+                       print(markdowner.convert(fin.read()), file=fout)
             except:
-                print(traceback.get_exc())
+                traceback.print_exc()
                 return "error"
 
     return "ok"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
