60657f76758791a1c33b1e31f6888f4e14c7b33d
Konstantin Amelichev <kostya.amelichev@gmail.com>
2022-08-14 16:10:16 +0400

    use only markdown materials

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3 -m pip install -r requirements.txt
-python3 clean.py
-nohup python3 main.py >>logfile.log 2>>errfile.log </dev/null &
-echo "$! flask" >>~/processes_web
-nohup ./caddy_linux_amd64_custom reverse-proxy --from kik0s.ru --to localhost:5000 &
-echo "$! caddy" >>~/processes_web
-nohup python3 autoreload.py >>logfile.log 2>>errfile.log </dev/null &
-echo "$! healthcheck" >>~/processes_web
